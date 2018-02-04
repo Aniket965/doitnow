a2e7a6667d02767bbc81649dd389a460ad916b3c
aniket965 <aniket965.as@gmail.com>
2018-02-04 14:15:21 +0530

    lol🐄

--- scripts/default.sh
+++ scripts/default.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 cd /Users/root1/Projects/
 cd $1
-code .
-exit 0
+if [ "$?" = 0 ]
+then
+    code .
+    exit 0
+else
+    echo "Name of Project is wrong 😅 have look!"
+    ls
+    exit 0
+fi
